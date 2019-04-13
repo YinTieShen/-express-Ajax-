b68c94e525b8d9351146f256626d0e279ee68faa
YinTieShen <1370436652@qq.com>
2019-04-13 11:36:41 +0800

    增加不规则瀑布流

--- routes/index.js
+++ routes/index.js
@@ -10,19 +10,22 @@ router.get('/', function(req, res, next) {
 router.get('/pubu',function(req,res,next){
   res.render('pubu');
 })
+router.get('/waterfall',function(req,res,next){
+  res.render('waterfall');
+})
 
 router.get('/data',function(req,res,next){
   var data = [
-      {id:1,src:"/images/1.jpg",des:"敢于行动"},
-      {id:1,src:"/images/1.jpg",des:"敢于行动"},
-      {id:1,src:"/images/1.jpg",des:"敢于行动"},
-      {id:1,src:"/images/1.jpg",des:"敢于行动"},
-      {id:1,src:"/images/1.jpg",des:"敢于行动"},
-      {id:1,src:"/images/1.jpg",des:"敢于行动"},
-      {id:1,src:"/images/1.jpg",des:"敢于行动"},
-      {id:1,src:"/images/1.jpg",des:"敢于行动"},
-      {id:1,src:"/images/1.jpg",des:"敢于行动"},
-      {id:1,src:"/images/1.jpg",des:"敢于行动"}
+      {id:1,src:"/images/1.jpg",des:"竹林"},
+      {id:2,src:"/images/2.jpg",des:"建筑"},
+      {id:3,src:"/images/3.jpg",des:"花"},
+      {id:4,src:"/images/4.jpg",des:"古迹"},
+      {id:5,src:"/images/5.jpg",des:"小桥"},
+      {id:6,src:"/images/6.jpg",des:"花"},
+      {id:7,src:"/images/7.jpg",des:"动漫"},
+      {id:8,src:"/images/8.jpg",des:"树林"},
+      {id:9,src:"/images/9.jpg",des:"建筑"},
+      {id:10,src:"/images/10.jpg",des:"红花"}
   ];
   res.json(data);
 
